421aef9d2bbe9274b3996cb231190af5385e041e
stschmidt <stschmidt@expediagroup.com>
2025-05-08 11:15:55 -0500

    loop

--- src/main/kotlin/com/cashngun/Application.kt
+++ src/main/kotlin/com/cashngun/Application.kt
@@ -1,14 +1,11 @@
 package com.cashngun
 
 fun main() {
-  var round = 1
   val lootDeck = INITIAL_DECK
 
   while(!lootDeck.isEmpty()) {
     val lootForThisRound = getLootForThisRound(deck)
 
     println("deck size: ${deck.size}")
-
-    round++
   }
 }

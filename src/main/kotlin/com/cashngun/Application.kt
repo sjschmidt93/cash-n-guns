e4f941236e8b3d16b1dbc727a49640daa607362c
Frank McCready <fmccready@expediagroup.com>
2025-05-08 12:06:42 -0500

    feat: implement players choose bullet cards

--- src/main/kotlin/com/cashngun/Application.kt
+++ src/main/kotlin/com/cashngun/Application.kt
@@ -2,16 +2,34 @@ package com.cashngun
 
 fun main() {
   val lootDeck = INITIAL_DECK
-  val numberOfPlayers = 2
+  val numberOfPlayers = 3
 
   val players = listOf(
-    Player("Frank"),
-    Player("Dan")
+    Player(name = "Frank"),
+    Player(name = "Dan"),
+    Player(name = "Steven")
   )
 
   while(!lootDeck.isEmpty()) {
     val lootForThisRound = getLootForThisRound(lootDeck)
+    println(lootForThisRound)
 
-    playersChooseBulletCards()
+    val choices = playersChooseBulletCards(players)
+    println(choices)
   }
+}
+
+fun playersChooseBulletCards(players: List<Player>): List<BulletCard> {
+  println("Choose your action:")
+  println("1) Bullet")
+  println("2) Click")
+  
+  val choices = players.map {
+    println("${it.name} chose your card")
+    when (readLine()?.trim()) {
+      "1" -> BulletCard.BANG
+      else -> BulletCard.CLICK
+    }
+  }
+  return choices
 }
--- src/main/kotlin/com/cashngun/Models.kt
+++ src/main/kotlin/com/cashngun/Models.kt
@@ -1,3 +1,4 @@
+package com.cashngun
 
 data class Player(
   val health: Int = 3,
